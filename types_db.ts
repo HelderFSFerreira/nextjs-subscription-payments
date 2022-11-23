557f462aaa4420321aabfaaa6dda869b86f2bfa7
Helder Ferreira <me@helderferreira.io>
2022-11-23 18:50:40 +0000

    init commit

--- types_db.ts
+++ types_db.ts
@@ -4,235 +4,236 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[];
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
-      users: {
-        Row: {
-          id: string;
-          first_name: string | null;
-          last_name: string | null;
-          billing_address: Json | null;
-          payment_method: Json | null;
-          full_name: string | null;
-          avatar_url: string | null;
-        };
-        Insert: {
-          id: string;
-          first_name?: string | null;
-          last_name?: string | null;
-          billing_address?: Json | null;
-          payment_method?: Json | null;
-          full_name?: string | null;
-          avatar_url?: string | null;
-        };
-        Update: {
-          id?: string;
-          first_name?: string | null;
-          last_name?: string | null;
-          billing_address?: Json | null;
-          payment_method?: Json | null;
-          full_name?: string | null;
-          avatar_url?: string | null;
-        };
-      };
       customers: {
         Row: {
-          id: string;
-          stripe_customer_id: string | null;
-        };
+          id: string
+          stripe_customer_id: string | null
+        }
         Insert: {
-          id: string;
-          stripe_customer_id?: string | null;
-        };
+          id: string
+          stripe_customer_id?: string | null
+        }
         Update: {
-          id?: string;
-          stripe_customer_id?: string | null;
-        };
-      };
-      products: {
-        Row: {
-          id: string;
-          active: boolean | null;
-          name: string | null;
-          description: string | null;
-          access_role:
-            | Database['public']['Enums']['content_access_role']
-            | null;
-          image: string | null;
-          metadata: Json | null;
-        };
-        Insert: {
-          id: string;
-          active?: boolean | null;
-          name?: string | null;
-          description?: string | null;
-          access_role?:
-            | Database['public']['Enums']['content_access_role']
-            | null;
-          image?: string | null;
-          metadata?: Json | null;
-        };
-        Update: {
-          id?: string;
-          active?: boolean | null;
-          name?: string | null;
-          description?: string | null;
-          access_role?:
-            | Database['public']['Enums']['content_access_role']
-            | null;
-          image?: string | null;
-          metadata?: Json | null;
-        };
-      };
-      posts: {
-        Row: {
-          id: number;
-          title: string;
-          content: string;
-          created_at: string | null;
-          access_level:
-            | Database['public']['Enums']['content_access_role']
-            | null;
-        };
-        Insert: {
-          id?: number;
-          title: string;
-          content: string;
-          created_at?: string | null;
-          access_level?:
-            | Database['public']['Enums']['content_access_role']
-            | null;
-        };
-        Update: {
-          id?: number;
-          title?: string;
-          content?: string;
-          created_at?: string | null;
-          access_level?:
-            | Database['public']['Enums']['content_access_role']
-            | null;
-        };
-      };
+          id?: string
+          stripe_customer_id?: string | null
+        }
+      }
       prices: {
         Row: {
-          id: string;
-          product_id: string | null;
-          active: boolean | null;
-          unit_amount: number | null;
-          currency: string | null;
-          type: Database['public']['Enums']['pricing_type'] | null;
-          interval: Database['public']['Enums']['pricing_plan_interval'] | null;
-          interval_count: number | null;
-          trial_period_days: number | null;
-          metadata: Json | null;
-          description: string | null;
-        };
+          id: string
+          product_id: string | null
+          active: boolean | null
+          description: string | null
+          unit_amount: number | null
+          currency: string | null
+          type: Database["public"]["Enums"]["pricing_type"] | null
+          interval: Database["public"]["Enums"]["pricing_plan_interval"] | null
+          interval_count: number | null
+          trial_period_days: number | null
+          metadata: Json | null
+        }
         Insert: {
-          id: string;
-          product_id?: string | null;
-          active?: boolean | null;
-          unit_amount?: number | null;
-          currency?: string | null;
-          type?: Database['public']['Enums']['pricing_type'] | null;
-          interval?:
-            | Database['public']['Enums']['pricing_plan_interval']
-            | null;
-          interval_count?: number | null;
-          trial_period_days?: number | null;
-          metadata?: Json | null;
-          description?: string | null;
-        };
+          id: string
+          product_id?: string | null
+          active?: boolean | null
+          description?: string | null
+          unit_amount?: number | null
+          currency?: string | null
+          type?: Database["public"]["Enums"]["pricing_type"] | null
+          interval?: Database["public"]["Enums"]["pricing_plan_interval"] | null
+          interval_count?: number | null
+          trial_period_days?: number | null
+          metadata?: Json | null
+        }
         Update: {
-          id?: string;
-          product_id?: string | null;
-          active?: boolean | null;
-          unit_amount?: number | null;
-          currency?: string | null;
-          type?: Database['public']['Enums']['pricing_type'] | null;
-          interval?:
-            | Database['public']['Enums']['pricing_plan_interval']
-            | null;
-          interval_count?: number | null;
-          trial_period_days?: number | null;
-          metadata?: Json | null;
-          description?: string | null;
-        };
-      };
+          id?: string
+          product_id?: string | null
+          active?: boolean | null
+          description?: string | null
+          unit_amount?: number | null
+          currency?: string | null
+          type?: Database["public"]["Enums"]["pricing_type"] | null
+          interval?: Database["public"]["Enums"]["pricing_plan_interval"] | null
+          interval_count?: number | null
+          trial_period_days?: number | null
+          metadata?: Json | null
+        }
+      }
+      products: {
+        Row: {
+          id: string
+          active: boolean | null
+          name: string | null
+          description: string | null
+          image: string | null
+          metadata: Json | null
+        }
+        Insert: {
+          id: string
+          active?: boolean | null
+          name?: string | null
+          description?: string | null
+          image?: string | null
+          metadata?: Json | null
+        }
+        Update: {
+          id?: string
+          active?: boolean | null
+          name?: string | null
+          description?: string | null
+          image?: string | null
+          metadata?: Json | null
+        }
+      }
+      questionnnares: {
+        Row: {
+          question_id: number
+          user_id: number
+          created_at: string | null
+        }
+        Insert: {
+          question_id: number
+          user_id: number
+          created_at?: string | null
+        }
+        Update: {
+          question_id?: number
+          user_id?: number
+          created_at?: string | null
+        }
+      }
+      questions: {
+        Row: {
+          id: number
+          question: string | null
+          option1: string | null
+          option2: string | null
+          option3: string | null
+          option4: string | null
+          correct: string | null
+        }
+        Insert: {
+          id?: number
+          question?: string | null
+          option1?: string | null
+          option2?: string | null
+          option3?: string | null
+          option4?: string | null
+          correct?: string | null
+        }
+        Update: {
+          id?: number
+          question?: string | null
+          option1?: string | null
+          option2?: string | null
+          option3?: string | null
+          option4?: string | null
+          correct?: string | null
+        }
+      }
       subscriptions: {
         Row: {
-          id: string;
-          user_id: string;
-          status: Database['public']['Enums']['subscription_status'] | null;
-          metadata: Json | null;
-          price_id: string | null;
-          quantity: number | null;
-          cancel_at_period_end: boolean | null;
-          created: string;
-          current_period_start: string;
-          current_period_end: string;
-          ended_at: string | null;
-          cancel_at: string | null;
-          canceled_at: string | null;
-          trial_start: string | null;
-          trial_end: string | null;
-        };
+          id: string
+          user_id: string
+          status: Database["public"]["Enums"]["subscription_status"] | null
+          metadata: Json | null
+          price_id: string | null
+          quantity: number | null
+          cancel_at_period_end: boolean | null
+          created: string
+          current_period_start: string
+          current_period_end: string
+          ended_at: string | null
+          cancel_at: string | null
+          canceled_at: string | null
+          trial_start: string | null
+          trial_end: string | null
+        }
         Insert: {
-          id: string;
-          user_id: string;
-          status?: Database['public']['Enums']['subscription_status'] | null;
-          metadata?: Json | null;
-          price_id?: string | null;
-          quantity?: number | null;
-          cancel_at_period_end?: boolean | null;
-          created?: string;
-          current_period_start?: string;
-          current_period_end?: string;
-          ended_at?: string | null;
-          cancel_at?: string | null;
-          canceled_at?: string | null;
-          trial_start?: string | null;
-          trial_end?: string | null;
-        };
+          id: string
+          user_id: string
+          status?: Database["public"]["Enums"]["subscription_status"] | null
+          metadata?: Json | null
+          price_id?: string | null
+          quantity?: number | null
+          cancel_at_period_end?: boolean | null
+          created?: string
+          current_period_start?: string
+          current_period_end?: string
+          ended_at?: string | null
+          cancel_at?: string | null
+          canceled_at?: string | null
+          trial_start?: string | null
+          trial_end?: string | null
+        }
         Update: {
-          id?: string;
-          user_id?: string;
-          status?: Database['public']['Enums']['subscription_status'] | null;
-          metadata?: Json | null;
-          price_id?: string | null;
-          quantity?: number | null;
-          cancel_at_period_end?: boolean | null;
-          created?: string;
-          current_period_start?: string;
-          current_period_end?: string;
-          ended_at?: string | null;
-          cancel_at?: string | null;
-          canceled_at?: string | null;
-          trial_start?: string | null;
-          trial_end?: string | null;
-        };
-      };
-    };
+          id?: string
+          user_id?: string
+          status?: Database["public"]["Enums"]["subscription_status"] | null
+          metadata?: Json | null
+          price_id?: string | null
+          quantity?: number | null
+          cancel_at_period_end?: boolean | null
+          created?: string
+          current_period_start?: string
+          current_period_end?: string
+          ended_at?: string | null
+          cancel_at?: string | null
+          canceled_at?: string | null
+          trial_start?: string | null
+          trial_end?: string | null
+        }
+      }
+      users: {
+        Row: {
+          id: string
+          full_name: string | null
+          avatar_url: string | null
+          billing_address: Json | null
+          payment_method: Json | null
+        }
+        Insert: {
+          id: string
+          full_name?: string | null
+          avatar_url?: string | null
+          billing_address?: Json | null
+          payment_method?: Json | null
+        }
+        Update: {
+          id?: string
+          full_name?: string | null
+          avatar_url?: string | null
+          billing_address?: Json | null
+          payment_method?: Json | null
+        }
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      install_available_extensions_and_test: {
+        Args: Record<PropertyKey, never>
+        Returns: boolean
+      }
+    }
     Enums: {
-      content_access_role: 'free' | 'basic' | 'premium';
-      pricing_type: 'one_time' | 'recurring';
-      pricing_plan_interval: 'day' | 'week' | 'month' | 'year';
+      pricing_plan_interval: "day" | "week" | "month" | "year"
+      pricing_type: "one_time" | "recurring"
       subscription_status:
-        | 'trialing'
-        | 'active'
-        | 'canceled'
-        | 'incomplete'
-        | 'incomplete_expired'
-        | 'past_due'
-        | 'unpaid';
-    };
-  };
+        | "trialing"
+        | "active"
+        | "canceled"
+        | "incomplete"
+        | "incomplete_expired"
+        | "past_due"
+        | "unpaid"
+    }
+  }
 }
+
